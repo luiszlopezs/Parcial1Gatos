19bbfb2a5470d58f3651d4a33a0fdd7a56a8185f
hailen <soyhailen@gmail.com>
2025-05-09 19:53:05 -0500

    creacion de metodos en clase gatoDAO

--- Parcial1Gatos/src/main/java/edu/progavud/parcial1pa/modelo/GatoVO.java
+++ Parcial1Gatos/src/main/java/edu/progavud/parcial1pa/modelo/GatoVO.java
@@ -9,31 +9,33 @@ package edu.progavud.parcial1pa.modelo;
  * @author hailen
  */
 public class GatoVO {
-    
+
     private Raza raza;
     private int id;
     private String codigoEMS;
     private String nombre;
-    private String pelaje;
-    private String orejas;
-    private String nariz;
-    private String ojos;
+    private String descripcion;
+    private String razaString = "";
 
-    public GatoVO(Raza raza, int id, String codigoEMS, String nombre, String pelaje, String orejas, String nariz, String ojos) {
+    public GatoVO(Raza raza, int id, String codigoEMS, String nombre, String descripcion) {
         this.raza = raza;
         this.id = id;
         this.codigoEMS = codigoEMS;
         this.nombre = nombre;
-        this.pelaje = pelaje;
-        this.orejas = orejas;
-        this.nariz = nariz;
-        this.ojos = ojos;
-    }    
+        this.descripcion = descripcion;
 
-    
-    
+    }
 
     public GatoVO() {
+    }
+
+    public String obtenerRazaString() {
+        razaString = razaString.concat(raza.getNombreRaza() + "-");
+        razaString = razaString.concat(raza.getColorPelaje() + "-");
+        razaString = razaString.concat(raza.getPatron() + "-");
+        razaString = razaString.concat(raza.getColorOjos() + "-");
+        razaString = razaString.concat(raza.getCola());
+        return razaString;
     }
 
     public Raza getRaza() {
@@ -68,38 +70,24 @@ public class GatoVO {
         this.nombre = nombre;
     }
 
-    public String getPelaje() {
-        return pelaje;
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public void setPelaje(String pelaje) {
-        this.pelaje = pelaje;
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 
-    public String getOrejas() {
-        return orejas;
+    
+
+    public String getRazaString() {
+        return razaString;
     }
 
-    public void setOrejas(String orejas) {
-        this.orejas = orejas;
-    }
-
-    public String getNariz() {
-        return nariz;
-    }
-
-    public void setNariz(String nariz) {
-        this.nariz = nariz;
-    }
-
-    public String getOjos() {
-        return ojos;
-    }
-
-    public void setOjos(String ojos) {
-        this.ojos = ojos;
+    public void setRazaString(String razaString) {
+        this.razaString = razaString;
     }
     
     
-    
+
 }
